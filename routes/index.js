f0c27a8542f6db0644a0ddb5f0a6b16dd3aaa63c
Wolmer Martins <wolmermartins@hotmail.com>
2019-12-11 08:19:54 -0300

    ENH: Organizes pages header with includes

--- routes/index.js
+++ routes/index.js
@@ -18,19 +18,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/contacts', function(req, res, next) {
-  res.render('contact', { title: `Contato | ${TITLE}` });
+  res.render('contact', {
+    title: `Contato | ${TITLE}`,
+    h1: 'Diga um oi!',
+    background: 'images/img_bg_3.jpg'
+  });
 });
 
 router.get('/menus', function(req, res, next) {
-  res.render('menu', { title: `Menu | ${TITLE}` });
+  res.render('menu', {
+    title: `Menu | ${TITLE}`,
+    h1: 'Saboreie nosso menu!',
+    background: 'images/img_bg_1.jpg'
+  });
 });
 
 router.get('/reservations', function(req, res, next) {
-  res.render('reservation', { title: `Reservas | ${TITLE}` });
+  res.render('reservation', {
+    title: `Reservas | ${TITLE}`,
+    h1: 'Reserve uma Mesa!',
+    background: 'images/img_bg_2.jpg'
+  });
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: `Serviços | ${TITLE}`})
+  res.render('services', {
+    title: `Serviços | ${TITLE}`,
+    h1: 'É um prazer poder servir!',
+    background: 'images/img_bg_1.jpg'
+  });
 });
 
 module.exports = router;
