5c51c7ea10bfaf8ce775aadaaf973cb8392f7c6c
Wolmer Martins <wolmermartins@hotmail.com>
2019-12-11 07:06:24 -0300

    ENH: Displays menus dynamically from DB values

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const db = require('../inc/db');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.query('SELECT * FROM tb_menus ORDER BY title', (err, results) => {
+    if (err) return console.log(err);
+
+    res.render('index', {
+      title: 'Restaurante Saboroso!',
+      menus: results
+    });
+  });
 });
 
 module.exports = router;
