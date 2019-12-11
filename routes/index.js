848f9f5995f51d6a1b8771a645a4612dfa3fe04d
Wolmer Martins <wolmermartins@hotmail.com>
2019-12-11 07:43:55 -0300

    ENH: Creates routes to the other pages

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,34 @@ var router = express.Router();
 
 const db = require('../inc/db');
 
+const TITLE = 'Restaurante Saboroso!';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   db.query('SELECT * FROM tb_menus ORDER BY title', (err, results) => {
     if (err) return console.log(err);
 
     res.render('index', {
-      title: 'Restaurante Saboroso!',
+      title: TITLE,
       menus: results
     });
   });
+});
+
+router.get('/contacts', function(req, res, next) {
+  res.render('contact', { title: `Contato | ${TITLE}` });
+});
+
+router.get('/menus', function(req, res, next) {
+  res.render('menu', { title: `Menu | ${TITLE}` });
+});
+
+router.get('/reservations', function(req, res, next) {
+  res.render('reservation', { title: `Reservas | ${TITLE}` });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: `Serviços | ${TITLE}`})
 });
 
 module.exports = router;
