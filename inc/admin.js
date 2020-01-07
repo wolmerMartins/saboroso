c53b150d2afcceac4221ce84b9736188f13ca353
Wolmer Martins <wolmermartins@hotmail.com>
2020-01-07 07:55:17 -0300

    ENH: Adds routes to menu

---
+++ inc/admin.js
@@ -0,0 +1,46 @@
+module.exports = {
+    getMenus(req) {
+        let menus = [
+            {
+                text: 'Tela Inicial',
+                href: '/admin/',
+                icon: 'home',
+                active: false
+            },
+            {
+                text: 'Menu',
+                href: '/admin/menus',
+                icon: 'cutlery',
+                active: false
+            },
+            {
+                text: 'Reservas',
+                href: '/admin/reservations',
+                icon: 'calendar-check-o',
+                active: false
+            },
+            {
+                text: 'Contatos',
+                href: '/admin/contacts',
+                icon: 'comments',
+                active: false
+            },
+            {
+                text: 'Usuários',
+                href: '/admin/users',
+                icon: 'users',
+                active: false
+            },
+            {
+                text: 'E-mails',
+                href: '/admin/emails',
+                icon: 'envelope',
+                active: false
+            }
+        ];
+
+        menus.forEach(menu => `/admin${req.url}` === menu.href ? menu.active = true : '');
+
+        return menus;
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const admin = require('../inc/admin');
 const users = require('../inc/users');
 
 router.use(function(req, res, next) {
@@ -11,16 +12,27 @@ router.use(function(req, res, next) {
     next();
 });
 
+router.use(function(req, res, next) {
+    req.menus = admin.getMenus(req);
+    next();
+});
+
 router.get('/contacts', function(req, res, next) {
-    res.render('admin/contacts');
+    res.render('admin/contacts', {
+        menus: req.menus
+    });
 });
 
 router.get('/emails', function(req, res, next) {
-    res.render('admin/emails');
+    res.render('admin/emails', {
+        menus: req.menus
+    });
 });
 
 router.get('/', function(req, res, next) {
-    res.render('admin/index');
+    res.render('admin/index', {
+        menus: req.menus
+    });
 });
 
 router.get('/login', function(req, res, next) {
@@ -44,17 +56,22 @@ router.get('/logout', function(req, res, next) {
 });
 
 router.get('/menus', function(req, res, next) {
-    res.render('admin/menus');
+    res.render('admin/menus', {
+        menus: req.menus
+    });
 });
 
 router.get('/reservations', function(req, res, next) {
     res.render('admin/reservations', {
+        menus: req.menus,
         date: {}
     });
 });
 
 router.get('/users', function(req, res, next) {
-    res.render('admin/users');
+    res.render('admin/users', {
+        menus: req.menus
+    });
 });
 
 
