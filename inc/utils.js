9184c4b95873be6e0dc1972800d62dd498397b4e
Wolmer Martins <wolmermartins@hotmail.com>
2020-01-13 08:31:58 -0300

    ENH: Adds update dbObj constructor

--- inc/utils.js
+++ inc/utils.js
@@ -1,4 +1,14 @@
 class Utils {
+    static setFieldsToUpdate(fields) {
+        let update = [];
+        
+        for (let field in fields) {
+            if (fields[field]) update.push(`${field} = ?`);
+        }
+
+        return update.join(',');
+    }
+
     static setColumns(fields) {
         let columns = [];
         fields.map(field => columns.push('?'));
@@ -7,16 +17,28 @@ class Utils {
 
     static setValuesToPersist(body) {
         let values = [];
-        Object.keys(body).map(key => values.push(body[key]));
+        Object.keys(body).map(key => body[key] ? values.push(body[key]) : '');
         return values;
     }
 
-    static setDBObject(table, fields, body) {
-        return {
-            table,
-            fields: fields.join(','),
-            values: Utils.setValuesToPersist(body),
-            columns: Utils.setColumns(fields)
+    static setDBObject(type, table, fields, body) {
+        if (type === 'save') {
+            return {
+                table,
+                fields: fields.join(','),
+                values: Utils.setValuesToPersist(body),
+                columns: Utils.setColumns(fields)
+            }
+        } else if (type === 'update') {
+            const id = fields.id;
+            delete fields.id;
+            
+            return {
+                id,
+                table,
+                update: Utils.setFieldsToUpdate(fields),
+                values: Utils.setValuesToPersist(fields)
+            }
         }
     }
 
