645021e7fc0c5a0d80fab8a6fd5e856917ba07a2
Wolmer Martins <wolmermartins@hotmail.com>
2020-01-15 07:20:57 -0300

    ENH: Adds a delete route to menus

--- routes/admin.js
+++ routes/admin.js
@@ -79,6 +79,12 @@ router.post('/menus', function(req, res, next) {
         .catch(err => res.send(err));
 });
 
+router.delete('/menus/:id', function(req, res, next) {
+    menu.delete(req.params.id)
+        .then(results => res.send(results))
+        .catch(err => res.send(err));
+});
+
 router.get('/reservations', function(req, res, next) {
     res.render('admin/reservations', admin.getParams(req, {
         date: {}
