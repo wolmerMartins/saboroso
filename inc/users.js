feb7598392549dfae750a4f4c83b1af1537c8584
Wolmer Martins <wolmermartins@hotmail.com>
2020-01-29 08:32:41 -0300

    ENH: Add getUsers method to list all register users

--- inc/users.js
+++ inc/users.js
@@ -2,6 +2,7 @@ const controller = require('./controller');
 
 const PAGE = 'admin/login';
 const TITLE = 'Login';
+const TABLE = 'tb_users';
 
 module.exports = {
     render(req, res, message, error) {
@@ -9,5 +10,8 @@ module.exports = {
     },
     login(body) {
         return controller.login(body);
+    },
+    getUsers() {
+        return controller.getData(TABLE, 'name');
     }
 }
