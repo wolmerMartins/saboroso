b37583acfb7f3cee2cb856f7a958fcedf8e5a72b
Wolmer Martins <wolmermartins@hotmail.com>
2020-03-15 10:58:34 -0300

    ENH: Adds the chart method to controller

--- inc/controller.js
+++ inc/controller.js
@@ -1,7 +1,26 @@
+const moment = require('moment');
+
 const db = require('./db');
+const Utils = require('./utils');
 const Pagination = require('./Pagination');
 
 let pag = null;
+
+
+const setDateFilter = dates => {
+    if (
+        Utils.isEmptyJson(dates)
+        || (!dates.start && !dates.end)
+    ) return '';
+
+    const { start, end } = dates;
+
+    if (!start) return `WHERE date <= '${moment(end).format('YYYY-MM-DD')}'`;
+    if (!end) return `WHERE date >= '${moment(start).format('YYYY-MM-DD')}'`;
+
+    return `WHERE date BETWEEN '${moment(start).format('YYYY-MM-DD')}'
+        AND '${moment(end).format('YYYY-MM-DD')}'`;
+};
 
 module.exports = {
     render(req, res, page, title, h1, bg, message, error) {
@@ -57,5 +76,30 @@ module.exports = {
         if (!pag) pag = new Pagination({ table, orderBy });
 
         return pag.getPage(page, perPage, dateStart, dateEnd);
+    },
+    chart(dates) {
+        const dateFilter = setDateFilter(dates);
+
+        return new Promise((resolve, reject) => {
+            db.query(`SELECT CONCAT(YEAR(date),'-',MONTH(date)) AS date, COUNT(*) AS total,
+                SUM(people) / COUNT(*) AS avg_people
+                FROM tb_reservations ${dateFilter}
+                GROUP BY CONCAT(YEAR(date),'-',MONTH(date))
+                ORDER BY CONCAT(YEAR(date),'-',MONTH(date)) DESC`,
+                [], (err, results) => {
+                    if (err) return reject(err);
+
+                    const months = [];
+                    const values = [];
+                    const resultsLimit = results.length;
+
+                    for (let result = 0; result < resultsLimit; result++) {
+                        months.push(moment(results[result].date).format('MMM-YYYY'));
+                        values.push(results[result].total);
+                    }
+
+                    resolve({ months, values });
+                });
+        });
     }
 }
